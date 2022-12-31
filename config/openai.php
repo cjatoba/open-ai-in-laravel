21afbb9ba6368b145741ea7d2963571b2120a086
cjatoba <claytonjatoba@gmail.com>
2022-12-31 08:50:49 -0300

    Install openai-php/laravel/package and change openai.php file in config folder

--- config/openai.php
+++ config/openai.php
@@ -1,5 +1,18 @@
 <?php
 
 return [
-  'OPENAI_API_KEY' => env('OPENAI_API_KEY')
+
+    /*
+    |--------------------------------------------------------------------------
+    | OpenAI API Key and Organization
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify your OpenAI API Key and organization. This will be
+    | used to authenticate with the OpenAI API - you can find your API key
+    | and organization on your OpenAI dashboard, at https://openai.com.
+    */
+
+    'api_key' => env('OPENAI_API_KEY'),
+    'organization' => env('OPENAI_ORGANIZATION'),
+
 ];
